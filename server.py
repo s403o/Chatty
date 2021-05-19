54cd49d260eb76cdebab748d5ffb53e189978834
s403o <eslam.adel.me@gmail.com>
2021-05-20 00:18:09 +0200

    init server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.host = 'localhost'
+app.debug = True
+socketio = SocketIO(app, cors_allowed_origins="*") # create server & fix cors errors
+
+@socketio.on(message)
+def message(msg):
+  print('Message: ', msg)
+  send(msg, broadcast=True) # send to everyone
+  return None
+
+if __name__ == '__main__':
+  socketio.run(app)
